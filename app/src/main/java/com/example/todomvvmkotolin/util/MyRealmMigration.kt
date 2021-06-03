e4565cf668616dbbcd5c16dc800d35768478e020
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2021-06-03 21:42:40 +0900

    update

---
+++ app/src/main/java/com/example/todomvvmkotolin/util/MyRealmMigration.kt
@@ -0,0 +1,17 @@
+package com.example.todomvvmkotolin.util
+
+import io.realm.DynamicRealm
+import io.realm.RealmMigration
+
+class MyRealmMigration: RealmMigration {
+    override fun migrate(realm: DynamicRealm, oldVersion: Long, newVersion: Long) {
+        val realmSchema = realm.schema
+        var oldVersion = oldVersion
+
+        if (oldVersion == 0L) {
+            realmSchema.get("TodoItem")!!
+                .removeField("isDelete")
+            oldVersion++
+        }
+    }
+}
